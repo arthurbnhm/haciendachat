a5ac8bcb68128b95207f241295a1b49769ed09be
Arthur <129552454+arthurbnhm@users.noreply.github.com>
2024-10-15 01:23:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -246,6 +246,27 @@ def oauth_callback(
         return default_user
     return None
 
+# Définir les Starters
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Résumer les conversations IA de la semaine",
+            message="Peux-tu me résumer les conversations autour de l'IA cette semaine ?",
+            icon="/public/write.svg",  
+        ),
+        cl.Starter(
+            label="Liens intéressants partagés",
+            message="Donne moi les liens intéressants qui ont été partagés",
+            icon="/public/learn.svg",
+        ),
+        cl.Starter(
+            label="Nouveaux produits sur Roast me I'm famous",
+            message="Quels produits ont été lancés récemment sur Roast me I'm famous ?",
+            icon="/public/idea.svg", 
+        ),
+    ]
+
 # Récupérer le port de l'environnement
 port = PORT
 
